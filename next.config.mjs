2bbd549c8d85bb0256aa052946a8d17328e98b40
Puripan <chopun2547@gmail.com>
2026-02-24 21:52:56 +0200

    Configure for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/My-Portfolio",
+  assetPrefix: "/My-Portfolio/",
   typescript: {
     ignoreBuildErrors: true,
   },
